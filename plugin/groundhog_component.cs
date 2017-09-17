d187eb81826d1b29c2c1b5a150980a6bc068ba47
Philip Belesky <contact@philipbelesky.com>
2017-09-17 21:51:34 +1000

    Base sentry WIP

---
+++ plugin/groundhog_component.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Grasshopper.Kernel;
+
+namespace groundhog
+{
+
+    public static class Globals // Global variable
+    {
+        public static bool Logged = false; // Keep track of whether a log has been issued
+    }
+
+    public abstract class GroundHog_Component : GH_Component
+    {
+
+        public override bool Read(GH_IO.Serialization.GH_IReader reader)
+        {
+
+            Console.WriteLine("Parent Constructor.");
+            if (Globals.Logged == false)
+            {
+                Globals.Logged = true;
+                Console.WriteLine("Log things");
+            }
+            else
+            {
+
+                Console.Write("Already Log things");
+            }
+
+            return base.Read(reader);
+        }
+
+    }
+
+}
