6276c0f0714d4e0fc20aa10f8e6bbc6b565162b0
Philip <contact@philipbelesky.com>
2017-09-21 20:39:20 +1000

    Assign new icon to grid; rename some methods

--- plugin/Mapping/groundhogFieldVisualisationGridComponent.cs
+++ plugin/Mapping/groundhogFieldVisualisationGridComponent.cs
@@ -142,15 +142,7 @@ namespace groundhog
         /// <summary>
         /// Provides an Icon for the component.
         /// </summary>
-        protected override System.Drawing.Bitmap Icon
-        {
-            get
-            {
-                //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
-            }
-        }
+        protected override Bitmap Icon => Resources.icon_field_grid_vis;
 
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
