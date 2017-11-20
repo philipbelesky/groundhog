0227455b9c096eac5826851ab441d934d52bcbc0
Philip Belesky <contact@philipbelesky.com>
2017-11-20 14:38:43 +1100

    Add pipeline for extracting component details

--- site/compile.sh
+++ site/compile.sh
@@ -3,11 +3,23 @@
 # Zip up all the files and move them to the Jekyll assets folder for publishing
 # Designed to be run from the site/Jekyll directory
 
+# Build
+# =====
+MSBuild ../plugin/groundhog.csproj /property:Configuration=Release
+
+# Extract Input/Output Parameters
+# ===============================
+rm -rf ../site/_data/components/ && mkdir ../site/_data/components/ # Delete old files
+python ../docs/extract_params.py
 
 # Plugin Files
 # ============
 zip -r -j ./downloads/plugin/groundhog.zip ../plugin/release/
 
+# Plugin Icons
+# ============
+
+cp ../plugin/Resources/**.png ./assets/plugin/icons/
 
 # Project Files
 # =============
