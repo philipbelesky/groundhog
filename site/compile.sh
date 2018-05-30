63a53f303f2314878187a825a8629cc93179fd05
Philip Belesky <contact@philipbelesky.com>
2018-05-30 11:20:10 +0100

    Update build steps
    Signed-off-by: Philip Belesky <contact@philipbelesky.com>

--- site/compile.sh
+++ site/compile.sh
@@ -8,34 +8,37 @@
 # ===============================
 rm -rf ../site/_data/components/*.json # Delete old files
 python ../docs/extract_params.py
-
+echo "Extracted Component Paramters"
 
 # Plugin Icons
 # ============
 rm -rf ./assets/plugin/icons/*.png
 cp ../plugin/Resources/**.png ./assets/plugin/icons/
+echo "Extracted Component Icons"
 
 
-# Project Files
+# Documentation Files
 # =============
 for i in ../docs/*;
     # Only do stuff i fthey are a directory
-    do if [ -d $i ]; then zip -r -j "${i%/}.zip" "$i" -x "*.DS_Store"; fi
+    do if [ -d $i ]; then zip -r -q -j "${i%/}.zip" "$i" -x "*.DS_Store"; fi
 done
 
 rm -rf ./downloads/documentation/*.zip
 mv ../docs/**.zip ./downloads/documentation/
+echo "Moved Documentation Files"
 
 
-# Documentation Files
+# Project Files
 # ===================
 for i in ../projects/*;
     # Only do stuff i fthey are a directory
-    do if [ -d $i ]; then zip -r -j "${i%/}.zip" "$i" -x "*.DS_Store"; fi
+    do if [ -d $i ]; then zip -r -q -j "${i%/}.zip" "$i" -x "*.DS_Store"; fi
 done
 
 rm -rf ./downloads/projects/*.zip
 mv ../projects/**.zip ./downloads/projects/
+echo "Moved Project Files"
 
 
 # NPM Dependencies
@@ -46,7 +49,7 @@ cp ./node_modules/@ibm/type/fonts/Sans/web/woff/IBMPlexSans-Regular*.woff ./asse
 
 # Build
 # =====
-MSBuild ../plugin/groundhog.csproj /property:Configuration=Release /verbosity:m
+MSBuild ../plugin/groundhog.csproj /property:Configuration=Release /verbosity:q
 
 
 # Plugin Files
@@ -56,4 +59,5 @@ rm -f ../plugin/release/groundhog.pdb # Comes from VS build using release config
 mv -f ../plugin/release/groundhog.dll ../plugin/release/groundhog.gha
 rm -f ./downloads/plugin/groundhog.zip
 
-zip -r -j ./downloads/plugin/groundhog.zip ../plugin/release/ -x "*.DS_Store*" -x "*manifest.yml*"
+zip -r -q -j ./downloads/plugin/groundhog.zip ../plugin/release/ -x "*.DS_Store*" -x "*manifest.yml*"
+echo "Built and Moved Plugin Files"
