877dc43192e78e5bab3e223efab6f89e0e602693
Philip Belesky <contact@philipbelesky.com>
2020-08-05 13:32:30 +1000

    Add caching to image resizing

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp');
 var responsive = require('gulp-responsive');
+var changed = require('gulp-changed');
+var count = require('gulp-count');
 
+var img_resize_cache = './assets/resize_cache/'
 
 gulp.task('figures', function () {
   return gulp.src([
       './assets/documentation/**/!(thumbnail)*.{png,jpg,jpeg}',
       './assets/projects/**/!(thumbnail)*.{png,jpg,jpeg}',
       './assets/techniques/**/!(thumbnail)*.{png,jpg,jpeg}'
-    ]).pipe(responsive({
+    ])
+    .pipe(changed(img_resize_cache)) // Retrieves only the files modified since last run
+    .pipe(count('## figures to resize', {logEmpty: true}))
+    .pipe(gulp.dest(img_resize_cache)) // Save modified files with original names in cache dir
+    .pipe(responsive(
+      {
       '**/*.*': [
         {
           rename: {
@@ -102,59 +110,61 @@ gulp.task('figures', function () {
           },
         },
       ],
-    }, {
-      // Global configuration for all images
-      // The output quality for JPEG, WebP and TIFF output formats
-      quality: 70,
-      // Use progressive (interlace) scan for JPEG and PNG output
-      progressive: true,
-      // Zlib compression level of PNG output format
-      compressionLevel: 6,
-      // Strip all metadata
-      withMetadata: false,
-      // Skip sizes that are too big
-      withoutEnlargement: true,
-      skipOnEnlargement: false,
-      errorOnEnlargement: false
-    }))
-    .pipe(gulp.dest('_site/assets/img/'));
+      }, {
+        // Global configuration for all images
+        // The output quality for JPEG, WebP and TIFF output formats
+        quality: 70,
+        // Use progressive (interlace) scan for JPEG and PNG output
+        progressive: true,
+        // Zlib compression level of PNG output format
+        compressionLevel: 6,
+        // Strip all metadata
+        withMetadata: false,
+        // Skip sizes that are too big
+        withoutEnlargement: true,
+        skipOnEnlargement: false,
+        errorOnEnlargement: false
+      }
+     ))
+    .pipe(gulp.dest('_site/assets/img/')); // Save modified files with new names in output dir
 });
-
 
 // These have different quality and size formats (lower/smaller/singular)
 gulp.task('thumbnails', function () {
   return gulp.src([
-    './assets/documentation/**/thumbnail.{png,jpg,jpeg}',
-    './assets/projects/**/thumbnail.{png,jpg,jpeg}',
-    './assets/techniques/**/thumbnail.{png,jpg,jpeg}'
-  ]).pipe(responsive({
-    '**/*.*': [
-      {
-        width: 690,
-        rename: {
-          suffix: '-medium',
-          extname: '.jpg',
+      './assets/documentation/**/thumbnail.{png,jpg,jpeg}',
+      './assets/projects/**/thumbnail.{png,jpg,jpeg}',
+      './assets/techniques/**/thumbnail.{png,jpg,jpeg}'
+    ])
+    .pipe(changed(img_resize_cache)) // Retrieves only the files modified since last run
+    .pipe(count('## thumbnails to resize', {logEmpty: true}))
+    .pipe(gulp.dest(img_resize_cache)) // Save modified files with original names in cache dir
+    .pipe(responsive({
+      '**/*.*': [
+        {
+          width: 690,
+          rename: {
+            suffix: '-medium',
+            extname: '.jpg',
+          },
         },
-      },
-      {
-        width: 690,
-        rename: {
-          suffix: '-medium',
-          extname: '.webp',
-        },
-      }
-    ]
-  }, {
-    // Global configuration for all images
-    // The output quality for JPEG, WebP and TIFF output formats
-    quality: 70,
-    // Zlib compression level of PNG output format
-    compressionLevel: 6,
-    // Strip all metadata
-    withMetadata: false,
-  }))
-  .pipe(gulp.dest('_site/assets/img/'));
+        {
+          width: 690,
+          rename: {
+            suffix: '-medium',
+            extname: '.webp',
+          },      }
+      ]
+    }, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 70,
+      // Zlib compression level of PNG output format
+      compressionLevel: 6,
+      // Strip all metadata
+      withMetadata: false,
+    }))
+    .pipe(gulp.dest('_site/assets/img/')); // Save modified files with new names in output dir
 });
 
-
-gulp.task('images', gulp.series('thumbnails', 'figures'));
+gulp.task('images', gulp.series('figures', 'thumbnails'));
